2ee2a40d6fe188d8ea32fe6ee78169e1f50317a0
edeitrick <edeitrick@gmail.com>
2018-06-04 11:47:09 -0400

    Tinkering with addToCart()
    @erikrug @ilanbarr @therealias
    
    I got Python on my laptop and pulled in your project - fixed some basic syntax stuff but I don't know what "bs" or "session" are so unsure how to fix...

--- SneakerBot.py
+++ SneakerBot.py
@@ -1,12 +1,14 @@
 import requests
-import bs4
 import random
 import webbrowser
+import bs4
+
 
 
 # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)
 # Chrome/39.0.2171.95 Safari/537.36'}
 class SneakerBot:
+
     def URLGen(model,size):
         BaseSize=580
         Shoesize = (float(size) - 6.5) * 20
@@ -14,10 +16,56 @@ class SneakerBot:
         ShoeSizeCode = int(float(RawSize) + 580)
         URL = 'http://www.adidas.com/us/' + str(model) + '.html?forceSelSize=' + str(model) + '_' + str(ShoeSizeCode)
         return URL;
+
     Model = input('model #')
     Size = input('Size:  ')
     URL = URLGen(Model,float(Size))
     print(str(URL))
+
+    def addtocart(self):
+        Model = input('model #')
+        Size = input('Size:  ')
+        URL = self.URLGen(Model, float(Size))
+
+        response = session.get(URL, headers={'Upgrade-Insecure-Requests': '1'})
+        URL = response.URL
+        soup = bs(response.text, 'html.parser')
+        size_container = soup.find('select', {'name': Model})
+        Size = 'null'
+
+        try:
+            for values in size_container.find_all('option'):
+                if Size == values.string.strip():
+                    Size = values['value']
+                    break
+        except:
+            print('All sold out!')
+            return False, 'null', 'null'
+
+        payload = {
+            'Quantity': '1',
+            'ajax': 'true',
+            'layer': 'Add To Bag overlay',
+            'masterModel': Model,
+            'Model': Size
+        }
+        headers = {
+            'Accept': '*/*',
+            'Origin': 'http://www.adidas.com',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+        if Size != 'null':
+            URL = 'http://www.adidas.com' + '/on/demandware.store/Sites-adidas-US-site/en_US/Cart-MiniAddProduct'
+            response = session.post(URL, data=payload, headers=headers)
+            if response.status_code == 200:
+                print('Shoe was added to cart')
+                return True, URL, Size
+            else:
+                print('{} unavailable'.format(Size))
+                return False, URL, Size
+
+    addtocart()
+
 
     def CheckSizes(url,model):
         headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)AppleWebKit/537.36 (KHTML,like Gecko)Chrome/39.0.2171.95 Safari/537.36'}
@@ -33,46 +81,9 @@ class SneakerBot:
             print(str(model) + ' Size ' + str(size) + ' Available ')
 
     def Main(model, size):
-        url = URLGen(model, size)
-        CheckSizes(url, model)
+        url = model.URLGen(model, size)
+        model.CheckSizes(url, model)
 
-    def addtocart(self):
-        response = session.get(URL, headers={'Upgrade-Insecure-Requests': '1'})
-        URL = response.URL
-        soup = bs(response.text, 'html.parser')
-        size_container = soup.find('select', {'name': Model})
-        Size = 'null'
-
-        try:
-            for vlues in size_container.find_all('option'):
-                if size == values.string.strip():
-                    Size=values['value']
-                    break
-        except:
-            print('All sold out!')
-            return false, 'null', 'null'
-
-        payload = {
-            'Quantity': '1',
-            'ajax': 'true',
-            'layer': 'Add To Bag overlay',
-            'masterModel': Model,
-            'Model': Size
-        }
-        headers = {
-            'Accept': '*/*',
-            'Origin': 'http://www.adidas.com',
-            'X-Requested-With': 'XMLHttpRequest'
-        }
-        if size_val != 'null':
-            URL = 'http://www.adidas.com' + '/on/demandware.store/Sites-adidas-US-site/en_US/Cart-MiniAddProduct'
-            response = session.post(url, data=payload, headers=headers)
-            if response.status_code == 200:
-                print ('Shoe was added to cart')
-                return True, URL, Size
-            else:
-                print('{} unavailable'.format(Size))
-                return False, URL, Size
 
 
 
